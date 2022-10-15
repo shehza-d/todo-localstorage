dfe4098491ee9bab440cd29016ce751caac05eb0
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-15 21:29:52 +0500

    Pushing new Changes

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 let arrayOfTodos = JSON.parse(localStorage.getItem("todoList")) || [];
 
-
 //date function
 (() => {
-	document.querySelector("#date").textContent = `${moment().format("D MMM YYYY")}`;
-	let time= moment().format("h:mm:s a");
-	const timeDiv = document.querySelector("#time");
-	timeDiv.textContent = time;		
-	setInterval(() => {
-		time = moment().format("h:mm:s a");
-		timeDiv.textContent = time;		
-	}, 1000);
-  })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  document.querySelector("#date").textContent = `${moment().format(
+    "D MMM YYYY"
+  )}`;
+  let time = moment().format("h:mm:s a");
+  const timeDiv = document.querySelector("#time");
+  timeDiv.textContent = time;
+  setInterval(() => {
+    time = moment().format("h:mm:s a");
+    timeDiv.textContent = time;
+  }, 1000);
+})();
 
 const addTodo = (event) => {
   event.preventDefault();
 
   const todo = document.querySelector("#todoItem").value;
+  todo.trim();
+  if (todo.length > 40) {
+    alert("string can't be greater then 40");
+    return;
+  }
+  // if(!todo) console.log(`emty str`)
+  if (todo === "") return;
   arrayOfTodos.push(todo);
   localStorage.setItem("todoList", JSON.stringify(arrayOfTodos));
 
@@ -67,20 +58,15 @@ document.querySelector("#todoList").addEventListener("click", (event) => {
   //   event.target.style.decoration = "line-through";
 });
 
-
-
-
-
 //theme function
-const themeChange=()=>{
-	document.body.classList.toggle("darkMode");
-}
-
-
-
-
-
-
-
+const themeChange = () => {
+  if (document.body.classList == "lightMode") {
+    document.body.classList.add("darkMode");
+    document.body.classList.remove("lightMode");
+  } else {
+    document.body.classList.add("lightMode");
+    document.body.classList.remove("darkMode");
+  }
+};
 
 //new API key 4719f247a38849138501b4bc9b42699e
